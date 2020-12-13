19d9b42d9c4232f2268d5e50fdc4899cde3fc1d3
Vidhita565 <73418284+Vidhita565@users.noreply.github.com>
2020-12-13 15:09:21 +0530

    Add files via upload

--- block.js
+++ block.js
@@ -7,14 +7,25 @@ class Block  {
         this.height = height
         this.body = Bodies.rectangle(x,y,width,height,options)
         
-
+       this.visiblity=255;
         World.add(world,this.body)
 
     }
     display()  {
-        rectMode(CENTER)
-        fill(171,247,247)
-        rect(this.body.position.x,this.body.position.y,this.width,this.height)
+        console.log(this.body.speed)
+        if(this.body.speed<4){
+            rectMode(CENTER)
+            fill(171,247,247)
+            rect(this.body.position.x,this.body.position.y,this.width,this.height)
+          
+        }
+        else{
+            World.remove(world, this.body);
+        push();
+            this.Visiblity = this.Visiblity - 5;
+            tint(255,this.Visiblity);
+            pop();
+        }
 
     }
 }
